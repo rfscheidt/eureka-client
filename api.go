fb25d573cf5b6fbe711c8be184dcce6c80e1e323
rafael scheidt <rafael.scheidt@agcapital.com.br>
2021-11-11 17:11:08 -0300

    change internal packages

--- api.go
+++ api.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/xuanbo/requests"
+	"github.com/rfscheidt/eureka-client/requests"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/rfscheidt/eureka-client
 
 go 1.14
-
-require (
-	github.com/xuanbo/requests v0.0.1
-)
